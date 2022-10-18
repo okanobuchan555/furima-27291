bbeee7ad34d49ed23a6277db85e8d50bb93fada9
okanobuchan555 <70964459+okanobuchan555@users.noreply.github.com>
2022-10-19 05:08:24 +0900

    user_kanri修正2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,22 @@ class UsersController < ApplicationController
   def show
   end
   
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_day)
+  end
+end  
+
 end
