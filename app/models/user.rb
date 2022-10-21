3bf2937790c87dfeeed67cb5dc219f6785b61c8d
okanobuchan555 <70964459+okanobuchan555@users.noreply.github.com>
2022-10-21 22:43:16 +0900

    user_kanri 再々修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,10 @@ class User < ApplicationRecord
 
       # バリデーションの設定(空の文字列を保存させない為と一意性制約)
       validates :username,               presence: true
-      validates :email,                  presence: true, format: {with: /\A\S+@\S+\.\S+\z/ }
-      validates :password,               presence: true, format: {with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]{6,}\z/ } # ここが半角英数字混合で6文字以上の正規表現
-      validates :last_name,              presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ } # ここがユーザー本名全角の正規表現
+      validates :password,               presence: true, format: {with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]{6,}\z/ ,message: 'は半角英数を両方含む必要があります'} #ここが半角英数字混合で6文字以上の正規表現
+      validates :last_name,              presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } # ここがユーザー本名全角の正規表現
       validates :last_name_kana,         presence: true, format: {with: /\A[ァ-ヶー－]+\z/ } # ここがフリガナ全角の正規表現
-      validates :first_name,             presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ } # ここがユーザー本名全角の正規表現
+      validates :first_name,             presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } # ここがユーザー本名全角の正規表現
       validates :first_name_kana,        presence: true, format: {with: /\A[ァ-ヶー－]+\z/ } # ここがフリガナ全角の正規表現
       validates :birth_day,              presence: true
       
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,6 +22,12 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include "Email can't be blank"
       end
 
+      it "メールアドレスに@が含まれていない場合は登録できない" do
+        @user.email = 'hogehuga.com'
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Email is invalid"
+      end
+
       it "パスワードが空では登録できない" do
         @user.password = ''  # passwordの値を空にする
         @user.valid?
@@ -46,6 +52,34 @@ RSpec.describe User, type: :model do
         @user.password_confirmation = '00000'
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+      end
+
+      it "パスワードが半角英字のみでは登録できない" do
+        @user.password = 'asdzxc'
+        @user.password_confirmation = 'asdzxc'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password は半角英数を両方含む必要があります")
+      end
+
+      it "パスワードが半角数字のみでは登録できない" do
+        @user.password = '123456'
+        @user.password_confirmation = '123456'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password は半角英数を両方含む必要があります")
+      end
+
+      it "パスワードが全角では登録できない" do
+        @user.password = 'ああああああ'
+        @user.password_confirmation = 'ああああああ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password は半角英数を両方含む必要があります")
+      end
+
+      it "passwordとconfirmが一致していないと登録できない" do
+        @user.password = 'test123'
+        @user.password_confirmation ='test1234'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
 
       it "last_nameがない場合は登録できないこと" do
