0fc47e8c219414059bfe0fbdf14f74d6b4164ad2
okanobuchan555 <70964459+okanobuchan555@users.noreply.github.com>
2022-12-05 22:02:03 +0900

    items_shupin 再々完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_day])
   end 
+
+  def after_sign_in_path_for(resource)
+    new_item_path
+  end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,7 +24,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:item_name, :image, :content, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price)
+    params.require(:item).permit(:item_name, :image, :content, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id:current_user.id)
   end
 
   def move_to_index
