d05d80ee757a9b30f2bd93b7aa2a2d0e2ea96cd1
okanobuchan555 <70964459+okanobuchan555@users.noreply.github.com>
2022-12-12 08:39:15 +0900

    ichiran_hyouji完了

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,9 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index]
+  # before_action :set_item, only: [:edit, :update]
 
   def index
-    # @items = Item.all
+    @items = Item.all
   end
 
   def new
@@ -18,6 +19,14 @@ class ItemsController < ApplicationController
     end
   end
 
+  # def edit
+  # end
+
+  # def update
+  #   @item.update(item_params)
+  #   redirect_to root_path
+  # end
+
   # def show
   # end
 
@@ -32,4 +41,9 @@ class ItemsController < ApplicationController
       redirect_to new_user_session_path
     end
   end
+
+  # def set_item
+  #   @item = Item.find(params[:id])
+  # end
+
 end
