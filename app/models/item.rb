0e601274834de50d4a51ec5d7fd770d4328c5dad
okanobuchan555 <70964459+okanobuchan555@users.noreply.github.com>
2022-12-17 23:23:42 +0900

    detail_hyouji完成

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,11 +2,15 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   has_one_attached :image
   validates :item_name, :content, :image, :price, presence: true
-  validates :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, presence: true 
-  validates :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, numericality: { other_than: 1 } 
+  validates :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, presence: true, numericality: { other_than: 1 }  
   validates :price, numericality: { with: /\A[0-9]+\z/, only_integer: true, messege: "is not a number" } 
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, messege: "can't be blank" }                  
   # アソシエーション
   belongs_to :user, dependent: :destroy
+  belongs_to :category 
+  belongs_to :sales_status
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
   # has_one :buyer, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'items#index'  #この1行を追加
-  resources :items, only: [:new, :create] do
+  resources :items, only: [:new, :create, :show] do
   end
 end
