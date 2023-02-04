91c54b86affe8b8e3153d3b92c67688112d44a48
okanobuchan555 <70964459+okanobuchan555@users.noreply.github.com>
2023-02-04 17:36:06 +0900

    item_purchase完成7

--- app/models/donation.rb
+++
@@ -1,3 +0,0 @@
-class Donation < ApplicationRecord
-   belongs_to :order
-end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'items#index'  #この1行を追加
   resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
-  devise_for :orders
-  root to: 'orders#index'
-  resources :orders, only:[:index, :new, :create]
+  resources :orders, only:[:index, :create]
   end
 end
